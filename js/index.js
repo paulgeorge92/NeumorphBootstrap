0a6ec26ea891810623a2fce1145c36edc836d9f5
Paul George <dev@paul-george.me>
2021-01-27 15:01:29 +0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$("#slider-1").ionRangeSlider();
+$("#slider-2").ionRangeSlider({
+  skin: "modern",
+  type: "double",
+  grid: true,
+  min: 0,
+  max: 1000,
+  from: 200,
+  to: 800,
+  prefix: "Rs."
+});
